c792c072232abae95996ba49114461336690c890
abba <abba@nuclearinstruments.eu>
2022-12-29 21:15:07 +0100

    python example ratemeter + fw (DT1260)

--- examples/Python/Ratemeter/Console/main.py
+++ examples/Python/Ratemeter/Console/main.py
@@ -4,8 +4,14 @@ from scisdk.scisdk_defines import *
 
 sdk = SciSDK()
 
-# add new device
-res = sdk.AddNewDevice("usb:13250","dt1260", "./RegisterFile.json","board0")
+#DT1260
+res = sdk.AddNewDevice("usb:10500","dt1260", "./DT1260RegisterFile.json","board0")
+#DT5560
+#res = sdk.AddNewDevice("192.168.50.10:8888","DT5560", "./DT5560RegisterFile.json","board0")
+#DT5550
+#res = sdk.AddNewDevice("usb:11000","DT5550", "./DT5550RegisterFile.json","board0")
+#V2740
+#res = sdk.AddNewDevice("192.168.50.10","V2740", "./V2740RegisterFile.json","board0")
 
 if res != 0:
     print(sdk.s_error(res))
@@ -19,10 +25,11 @@ if res == 0:
     # read data
     res, buf = sdk.ReadData("board0:/MMCComponents/RateMeter_0", buf)
 
+    print("-------------------")
     # print data
     for i in range(0, buf.info.nchannels):
         print("Ratemeter channer",i,buf.data[i])
 
-    res = sdk.FreeBuffer("board0:/MMCComponents/RateMeter_0", 1, buf)
+    res = sdk.FreeBuffer("board0:/MMCComponents/RateMeter_0", buf)
     sdk.DetachDevice("board0")
     res = sdk.FreeLib()
