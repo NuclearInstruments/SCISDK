2a261544d70dc548ad7332563b005697fff2b7d6
abba <abba@nuclearinstruments.eu>
2022-12-31 09:28:57 +0100

    board driver dt1260

--- src/boards_driver/bd_dt1260.cpp
+++ src/boards_driver/bd_dt1260.cpp
@@ -15,17 +15,18 @@ bd_dt1260::bd_dt1260(SciSDK_HAL *hal, json j, string path) : SciSDK_Node(hal, j,
     board_params.offset.value = 2048;
     board_params.offset.address = 4294967289;
 
-    RegisterParameter("analog_offset", "set analog offset", SciSDK_Paramcb::Type::U32, this);
+    RegisterParameter("analog.offset", "set analog offset", SciSDK_Paramcb::Type::U32, this);
 }
 
 
-NI_RESULT bd_dt1260::ISetParamI32(string name, uint32_t value)
+NI_RESULT bd_dt1260::ISetParamU32(string name, uint32_t value)
 {
-	if (name == "analog_offset") {
+	if (name == "analog.offset") {
         if ((value > 4095) || (value<0)) {
             return NI_PARAMETER_OUT_OF_RANGE;
         } else {
             board_params.offset.value = value;
+            UpdateHardware();
             return NI_OK;
         }
     }
@@ -33,9 +34,9 @@ NI_RESULT bd_dt1260::ISetParamI32(string name, uint32_t value)
     return NI_INVALID_PARAMETER;
 }
 
-NI_RESULT bd_dt1260::IGetParamI32(string name, uint32_t *value)
+NI_RESULT bd_dt1260::IGetParamU32(string name, uint32_t *value)
 {
-    if (name == "analog_offset") {
+    if (name == "analog.offset") {
         *value = board_params.offset.value;
         return NI_OK;
     }
