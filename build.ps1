4b83132cc6cb0431e3284e92f91a564670edcefc
abba <abba@nuclearinstruments.eu>
2023-01-01 11:52:12 +0100

    cicd docs

--- build.ps1
+++ build.ps1
@@ -3,3 +3,11 @@ MSBuild SciSDK_VC++\SciSDK_VC++.sln -t:Rebuild -p:Configuration=Debug -p:Platfor
 MSBuild SciSDK_VC++\SciSDK_VC++.sln -t:Rebuild -p:Configuration=Release -p:Platform=x86
 MSBuild SciSDK_VC++\SciSDK_VC++.sln -t:Rebuild -p:Configuration=Debug -p:Platform=x86
 New-Item -Path "." -Name "output" -ItemType "directory" -Force
+Copy-Item ".\bin" -Destination ".\output\" -Recurse -Force -Exclude *.pdb,*.exe,*.exp
+New-Item -Path "." -Name "output\src" -ItemType "directory" -Force
+Copy-Item ".\src\NIErrorCode.h" -Destination ".\output\src\" -Force
+Copy-Item ".\src\SciSDK_DLL.h" -Destination ".\output\src\" -Force
+Copy-Item ".\src\scisdk_defines.h" -Destination ".\output\src\" -Force
+Copy-Item ".\examples" -Destination ".\output\examples\" -Force -Recurse
+
+Compress-Archive -Path ".\output\*" -DestinationPath ".\output\output.zip" -Force
