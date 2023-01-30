12246cb800899e930cabbb9741a4919fe1a39091
Andrea Abba <abba@nuclearinstruments.eu>
2023-01-30 11:05:27 +0000

    compile for zynq 1

---
+++ compile_for_zynq.sh
@@ -0,0 +1,23 @@
+pip install -i http://pypi-ni.lanni/simple --trusted-host pypi-ni.lanni nirelease
+
+ROOT=$(pwd)
+cd /tmp/
+python3 -m nirelease --pull -u R5560SE/rev-B-A/sub/fw-linux-sdk/r5560se-daq-sdk/
+cd r5560se-daq-sdk
+chmod +x sdk.sh
+./sdk.sh -y -d /tmp/sdk
+cd /tmp/sdk
+source  /tmp/sdk/environment-setup-cortexa9t2hf-neon-xilinx-linux-gnueabi
+
+cd $ROOT
+autoreconf -if
+mkdir build_zynq
+cd build_zynq
+../configure --host=arm-xilinx-linux-gnueabi --prefix=$(pwd)/output/
+make -j16
+make install
+cd output
+tar cvf ../scisdk-zynq.tar.gz .
+
+
+
