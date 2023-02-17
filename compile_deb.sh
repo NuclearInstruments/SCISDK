9d2fed349cef9f6d544d68eb8a75d264aa70b238
Andrea Abba <abba@nuclearinstruments.eu>
2023-02-17 16:03:45 +0000

    Update .gitlab-ci.yml, compile_deb.sh

---
+++ compile_deb.sh
@@ -0,0 +1,57 @@
+
+cd build_x64/output
+fpm \
+  -s dir -t deb \
+  -p ../../scisdk-x64.deb \
+  --name scisdk \
+  --license agpl3 \
+  --version $CI_COMMIT_TAG \
+  --architecture x86_64 \
+  --description "Nuclear Instruments SciCompiler SDK" \
+  --url "https://github.com/NuclearInstruments/SCISDK" \
+  --maintainer "Andrea Abba <abba@nuclearinstruments.eu>" \
+  .
+
+cd ../..
+
+cd build_i386/output
+fpm \
+  -s dir -t deb \
+  -p ../../scisdk-i386.deb \
+  --name scisdk \
+  --license agpl3 \
+  --version $CI_COMMIT_TAG \
+  --architecture all \
+  --description "Nuclear Instruments SciCompiler SDK" \
+  --url "https://github.com/NuclearInstruments/SCISDK" \
+  --maintainer "Andrea Abba <abba@nuclearinstruments.eu>" \
+  .
+cd ../..
+
+cd build_arm64/output
+fpm \
+  -s dir -t deb \
+  -p ../../scisdk-arm64.deb \
+  --name scisdk \
+  --license agpl3 \
+  --version $CI_COMMIT_TAG \
+  --architecture aarch64 \
+  --description "Nuclear Instruments SciCompiler SDK" \
+  --url "https://github.com/NuclearInstruments/SCISDK" \
+  --maintainer "Andrea Abba <abba@nuclearinstruments.eu>" \
+  .
+cd ../..
+
+cd build_armhf/output
+fpm \
+  -s dir -t deb \
+  -p ../../scisdk-armhf.deb \
+  --name scisdk \
+  --license agpl3 \
+  --version $CI_COMMIT_TAG \
+  --architecture armhf \
+  --description "Nuclear Instruments SciCompiler SDK" \
+  --url "https://github.com/NuclearInstruments/SCISDK" \
+  --maintainer "Andrea Abba <abba@nuclearinstruments.eu>" \
+  .
+cd ../..
