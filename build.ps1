83c00a95df54c81ff6ffd8052e30d12f285890f5
Stefano Carsi <s.carsi@nuclearinstruments.eu>
2025-12-04 18:35:17 +0100

    Add error handling

--- build.ps1
+++ build.ps1
@@ -21,21 +21,26 @@ $versionContent | Set-Content -Path "src/scisdk_version.h" -Force
 
 Write-Host "Veersion updated in src/scisdk_version.h a $version"
 
+try {
+    MSBuild SciSDK_VC++\SciSDK_VC++.sln -t:Rebuild -p:Configuration=Release -p:Platform=x64
+    MSBuild SciSDK_VC++\SciSDK_VC++.sln -t:Rebuild -p:Configuration=Debug -p:Platform=x64
+    MSBuild SciSDK_VC++\SciSDK_VC++.sln -t:Rebuild -p:Configuration=Release -p:Platform=x86
+    MSBuild SciSDK_VC++\SciSDK_VC++.sln -t:Rebuild -p:Configuration=Debug -p:Platform=x86
+    MSBuild wrapper\CSharp_SciSDK\CSharp_SciSDK.sln -t:Rebuild -p:Configuration=Release -p:Platform="Any CPU"
+    MSBuild wrapper\CSharp_SciSDK\CSharp_SciSDK.sln -t:Rebuild -p:Configuration=Debug -p:Platform="Any CPU"
 
-MSBuild SciSDK_VC++\SciSDK_VC++.sln -t:Rebuild -p:Configuration=Release -p:Platform=x64
-MSBuild SciSDK_VC++\SciSDK_VC++.sln -t:Rebuild -p:Configuration=Debug -p:Platform=x64
-MSBuild SciSDK_VC++\SciSDK_VC++.sln -t:Rebuild -p:Configuration=Release -p:Platform=x86
-MSBuild SciSDK_VC++\SciSDK_VC++.sln -t:Rebuild -p:Configuration=Debug -p:Platform=x86
-MSBuild wrapper\CSharp_SciSDK\CSharp_SciSDK.sln -t:Rebuild -p:Configuration=Release -p:Platform="Any CPU"
-MSBuild wrapper\CSharp_SciSDK\CSharp_SciSDK.sln -t:Rebuild -p:Configuration=Debug -p:Platform="Any CPU"
+    New-Item -Path "." -Name "output" -ItemType "directory" -Force
+    Copy-Item ".\bin" -Destination ".\output\" -Recurse -Force -Exclude *.pdb,*.exp
+    New-Item -Path "." -Name "output\src" -ItemType "directory" -Force
+    Copy-Item ".\src\NIErrorCode.h" -Destination ".\output\src\" -Force
+    Copy-Item ".\src\SciSDK_DLL.h" -Destination ".\output\src\" -Force
+    Copy-Item ".\src\scisdk_defines.h" -Destination ".\output\src\" -Force
+    Copy-Item ".\examples" -Destination ".\output\examples\" -Force -Recurse
+    Copy-Item ".\wrapper" -Destination ".\output\wrapper" -Force -Recurse
 
-New-Item -Path "." -Name "output" -ItemType "directory" -Force
-Copy-Item ".\bin" -Destination ".\output\" -Recurse -Force -Exclude *.pdb,*.exp
-New-Item -Path "." -Name "output\src" -ItemType "directory" -Force
-Copy-Item ".\src\NIErrorCode.h" -Destination ".\output\src\" -Force
-Copy-Item ".\src\SciSDK_DLL.h" -Destination ".\output\src\" -Force
-Copy-Item ".\src\scisdk_defines.h" -Destination ".\output\src\" -Force
-Copy-Item ".\examples" -Destination ".\output\examples\" -Force -Recurse
-Copy-Item ".\wrapper" -Destination ".\output\wrapper" -Force -Recurse
-
-Compress-Archive -Path ".\output\*" -DestinationPath ".\output\scisdk-full.zip" -Force
+    Compress-Archive -Path ".\output\*" -DestinationPath ".\output\scisdk-full.zip" -Force
+} catch {
+    Write-Host "Error during setup:"
+    Write-Host $_.Exception.Message
+    exit 1
+}
